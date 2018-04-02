90303543c62b007ee53479fb0005b290b540ec12
Ruslan Dovgospiniy <ruslan.dovgospiniy@symmetrics.de>
2018-04-02 17:35:49 +0300

    Add base admin part skeleton

---
+++ app/models.py
@@ -0,0 +1,123 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
+
+
+class Employee(UserMixin, db.Model):
+    """
+    Create an Employee table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'employees'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<Employee: {}>'.format(self.username)
+
+
+# Set up user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return Employee.query.get(int(user_id))
+
+
+class Department(db.Model):
+    """
+    Create a Department table
+    """
+
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='department',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
+
+
+class Role(db.Model):
+    """
+    Create a Role table
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
+
+
+class Public(db.Model):
+    """
+    Create a Publics table
+    """
+
+    __tablename__ = 'public'
+
+    id_public = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.String(200))
+    url = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer)
+    id_public_data = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Publics: {}>'.format(self.name)
+
+
+class Public_Data(db.Model):
+    """
+    Create a Public_Data table
+    """
+
+    __tablename__ = 'public_data'
+
+    id_public_data = db.Column(db.Integer, primary_key=True)
+    post_text = db.Column(db.String(200))
+    photo = db.Column(db.String(200))
+    video = db.Column(db.String(200))
+    post_file = db.Column(db.String(200))
+    music = db.Column(db.String(200))
+
+    def __repr__(self):
+        return '<Public_Data: {}>'.format(self.name)
